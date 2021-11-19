f739a53bbf1fc7cecd15426ed7a06f303a115c48
JuanBojorquez <48139166+JuanBojorquez@users.noreply.github.com>
2021-11-19 15:41:38 -0700

    Update settings.php

--- inc/settings.php
+++ inc/settings.php
@@ -10,7 +10,7 @@ if (empty($_SESSION["nombre"]))
 {
   echo "Se detecto un acceso ilegal al sistema, su ip esta siendo monitoreada y sera enviada a la policia";
   ?>
-  <a href="https://the-crud.herokuapp.com">Sitio de login</a>
+  <a href="https://el-crud.herokuapp.com/index.php">Sitio de login</a>
   <?php
   exit();
 }
